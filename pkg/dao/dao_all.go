972059f6aa7624dd1bfb4cfa4aa2be83fe1a7ee2
Yuan <431625039@qq.com>
2025-02-02 11:37:03 +0800

    dao层定义dao_all，可用于通用的CURD，只需要额外传表名与结构体。

---
+++ pkg/dao/dao_all.go
@@ -0,0 +1,59 @@
+package dao
+
+import (
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"golang.org/x/net/context"
+	"zhku-oj-server/pkg/utils"
+)
+
+//通用CURD  要传结构体、表名
+
+func (d *Dao) Create(ctx context.Context, tableName string, model interface{}) (id string, err error) {
+	res, err := d.mongo.InsertOne(ctx, tableName, model)
+	if err != nil {
+		return "", err
+	}
+	if res.InsertedID != nil {
+		return res.InsertedID.(primitive.ObjectID).Hex(), nil
+	}
+	return "", nil
+}
+
+func (d *Dao) GetList(ctx context.Context, tableName string, comQuery *utils.CommonQuery) (items *utils.RespPageQuery, err error) {
+	lg := utils.GetDefaultLogger()
+	items = &utils.RespPageQuery{
+		Items: make([]*map[string]interface{}, 0),
+	}
+	//query := bson.M{}
+	opts := utils.BuildMongoOptions(comQuery)
+	lg.Println(opts)
+	lg.Println(comQuery.Filters)
+	err = d.mongo.FindSome(ctx, tableName, comQuery.Filters, items, opts)
+	return
+}
+
+func (d *Dao) GetOne(ctx context.Context, tableName string, model interface{}, query interface{}) (interface{}, error) {
+	//打印日志
+	lg := utils.GetDefaultLogger()
+	lg.Println("查询条件：", query)
+	//按query条件查询
+	err := d.mongo.FindOne(ctx, tableName, query, &model)
+	return model, err
+}
+
+func (d *Dao) Delete(ctx context.Context, tableName string, selector bson.M) (err error) {
+	//打印日志
+	lg := utils.GetDefaultLogger()
+	lg.Println("删除：", selector)
+	_, err = d.mongo.Remove(ctx, tableName, selector)
+	return
+}
+
+func (d *Dao) Update(ctx context.Context, tableName string, selector bson.M, update bson.M) (err error) {
+	//打印日志
+	lg := utils.GetDefaultLogger()
+	lg.Println("修改：", selector, update)
+	_, err = d.mongo.Upsert(ctx, tableName, selector, update)
+	return
+}
